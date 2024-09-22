eec41b74a72afe078aa4d78d82aa60ec029bb6c3
salma <salmaelmaghawry91@gmail.com>
2024-09-22 08:07:11 +0300

    get category service done

---
+++ lib/services/get_categories_service.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:store_app/models/product_model.dart';
+
+class GetCategoriesService {
+  Future<List<ProductModel>> getCategoriesService(
+      {required String categoryName}) async {
+    http.Response response = await http.get(
+        Uri.parse("https://fakestoreapi.com/products/category/$categoryName"));
+    List<dynamic> data = jsonDecode(response.body);
+
+    List<ProductModel> productList = [];
+    for (int i = 0; i < data.length; i++) {
+      productList.add(
+        ProductModel.fromJson(data[i]),
+      );
+    }
+    return productList;
+  }
+}
